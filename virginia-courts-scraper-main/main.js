cc994e233feebeadf85b075866e293cf26eb7ba8
FreeMarketFred <fred.flores.dsdi@gmail.com>
2024-06-03 05:42:10 -0400

    Update main.js

--- virginia-courts-scraper-main/main.js
+++ virginia-courts-scraper-main/main.js
@@ -11,13 +11,15 @@ const { scraperMain, detailScraper } = require('./scrapers/index');
     !fs.existsSync(`./data`)?fs.mkdirSync(`./data/`):'';
     await fileSaver(`../data/${formattedOutputDate}.json`, addCourtNameData);
     const summary = await summarizeCodeSections(mainList);
-    // const userDecision = await userPrompt('Do you wish to continue with getting case data on specific codes? (yes/no): ');
-    // if (userDecision.toLowerCase() === 'yes') {
-    //     const setupData = await setup(`\nPlease enter the codes you wish to filter, separated by commas (for example, '18.2-266,18.2-101').\nAlternatively, just press 'Enter' to skip this step and continue.`);
-    //     if (setupData != false) await fileSaver(`./config.json`, { codeSections: setupData });
-    //     const filteredData = fs.existsSync('./config.json') ? await userFilter(mainList) : mainList;
-    //     const details = await detailScraper(filteredData);
-    //     await fileSaver(`./data/${formattedOutputDate}_details.json`, details);
-    // };
+    const userDecision = await userPrompt('Do you wish to continue with getting case data on specific codes? (yes/no): ');
+    if (userDecision.toLowerCase() === 'yes') {
+        const setupData = await setup(`\nPlease enter the codes you wish to filter, separated by commas (for example, '18.2-266,18.2-101').\nAlternatively, just press 'Enter' to skip this step and continue.`);
+        if (setupData != false) await fileSaver(`./config.json`, { codeSections: setupData });
+        await logFile("Search Query Codes", `${setupData}`);
+        const configExists = await fs.existsSync('./utils/config.json');
+        const filteredData = (configExists) ? await userFilter(mainList) : mainList;
+        const details = await detailScraper(filteredData);
+        await fileSaver(`../data/${formattedOutputDate}_details.json`, details);
+    };
     await logFile("FINISHED", `${formattedUserDate}`);
 })();
